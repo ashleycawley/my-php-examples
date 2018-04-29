a71bf283d13bba8ec4e5e3aed99968d913252af1
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-29 18:05:04 +0000

    init commit

---
+++ loops.php
@@ -0,0 +1,9 @@
+<?php
+
+// Create for loop to say hi Ashley 9 times
+
+// Create foreach loop to create strings telling us people and favorite colours
+
+// Create a coin flipping while loop looking for three heads
+
+?>
