2254dada151b60dd7636aeb0a5f5d3ec656bcf5e
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-29 15:50:22 +0000

    Added more comments

--- switch-statement.php
+++ switch-statement.php
@@ -6,7 +6,7 @@ switch ( $name )
 {
 case 'Ashley':
     echo 'Hey Ashley';
-    break;
+    break; # This stops the switch statement from proceeding on the next one
 
 case 'Justin':
     echo 'Hey Justin';
@@ -15,6 +15,9 @@ case 'Justin':
 case 'Tom':
     echo 'Hey Tom';
     break;
+
+default: # This handles any other names not specified above.
+    echo "Hey Anonymous";
 }
 
 ?>
