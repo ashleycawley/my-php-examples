d9f940ec44db98984b50c78a6016d6eff33728ea
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-29 14:55:23 +0000

    Correcting my mistakes; I forgot to added semicolons at the end of lines, I'm always doing that, sshhh I'm new here.

--- echo.php
+++ echo.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "hello world";
-echo "<br><br>" # This inserts HTML into the page (creating a line break)
-echo "I am hoping this line isn't on the same line as the message above."
+echo "<br><br>"; # This inserts HTML into the page (creating a line break)
+echo "I am hoping this line isn't on the same line as the message above.";
 
 ?>
