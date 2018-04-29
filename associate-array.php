b0cf6b8ce096496288077cc37b8f75cbe7f1c43a
Ashley Cawley <ash@ashleycawley.co.uk>
2018-04-29 16:50:14 +0000

    init commit

---
+++ associate-array.php
@@ -0,0 +1,12 @@
+<?php
+
+# An example of an associate array, you essentially create your own index or nicknames which
+# you can use later to retrieve items from the array
+$people = array(
+    'Dad'   =>  'Gerry',
+    'Mum'   =>  'Barbara'
+);
+
+echo $people['Dad'] # Note we aren't specifying a number in the array but instead a nickname from our own index
+
+?>
