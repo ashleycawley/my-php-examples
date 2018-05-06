e75f6a84af0a992e120f631d1df7fad931dd1ad2
Ashley Cawley <ash@ashleycawley.co.uk>
2018-05-06 15:08:54 +0000

    Added a simple function inside of functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+// Creates a simple function which echos a message
+function display_greeting() {
+
+    echo 'Hey Ashley.';
+}
+
+// Calls the function to be used
+display_greeting();
+
+?>
