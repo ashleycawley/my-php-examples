1ead796564cebd694f3ecc5d1391834011647113
Ashley Cawley <ash@ashleycawley.co.uk>
2018-05-06 15:18:12 +0000

    Created file

---
+++ functions-advanced.php
@@ -0,0 +1,18 @@
+<?php
+
+// THIS FILE IS INCOMPLETE - 7:42 into "functions" video.
+
+// Uses the coin flip code that I coded earlier
+function coin_flip() {
+
+    $flip = rand( 0, 1 );
+   if ( $flip )
+   {
+       echo "Heads<br>";
+   } else {
+       echo "Tails<br>";
+   }
+
+}
+
+?>
